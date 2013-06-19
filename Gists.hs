bf234920e24f4ddab89d018d62429ed0d003bf34
Lyndon Maydwell <lyndon@sordina.net>
2013-06-19 21:34:08 +1000

    Cleaning up

--- Gists.hs
+++ Gists.hs
@@ -5,6 +5,7 @@ import Text.HandsomeSoup
 import Network.HTTP.Conduit      (simpleHttp)
 import Data.ByteString.Lazy.UTF8 (toString)
 import System.Environment
+import Data.Tree.NTree.TypeDefs
 
 base :: String
 base = "https://gist.github.com"
@@ -24,19 +25,21 @@ help = putStrLn "Usage: gists <username>"
 run :: String -> IO ()
 run path = do
   bytes <- simpleHttp $ base ++ path
-  let
-    text = toString bytes
-    doc  = readString [withParseHTML yes, withWarnings no] text
+
+  let text = toString bytes
+      doc  = readString [withParseHTML yes, withWarnings no] text
 
   gists <- runX $ doc >>> getGists
-
   mapM_ (putStrLn . (base ++)) gists
-
   runX (doc >>> getNextPageLink) >>= mapM_ run . take 1
 
-getNextPageLink = css ".pagination a"       >>> filterA (this //> hasText (elem "Older" . words)) ! "href"
-getGists        = css ".gist-item"          >>> (link <+> description <+> snippet) >. unwords
-link            = css ".creator a"          >>. drop 1 >>> (getAttrValue "href" <+> deep getText) >. unwords
-snippet         = css ".line-data"          >>> deep getText >. strip
-description     = css ".description"        >>> deep getText >. strip
-strip           = unwords . words . unwords
+getNextPageLink, getGists, link, snippet, description :: ArrowXml cat => cat (NTree XNode) String
+
+getNextPageLink = css ".pagination a" >>> filterA (this //> hasText (elem "Older" . words)) ! "href"
+getGists        = css ".gist-item"    >>> (link <+> description <+> snippet) >. strip
+link            = css ".creator a"    >>. drop 1 >>> (getAttrValue "href" <+> deep getText) >. strip
+snippet         = css ".line-data"    >>> deep getText >. strip
+description     = css ".description"  >>> deep getText >. strip
+
+strip :: [String] -> String
+strip = unwords . words . unwords
